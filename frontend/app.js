b1d73b7f37034bef872c31fa69706f271929e68b
ZTLAB57 <ztlab57@ZTLAB57s-Mac-mini.local>
2026-05-20 13:01:25 +0530

    app.js updated

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Frontend Running Version 1 updated');
+    res.send('Frontend Running Version 1 updated with the build number');
 });
 
 app.listen(3000, () => {
